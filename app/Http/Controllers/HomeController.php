2c4897d9f9375e40e956af02f6e2ee8219e9651d
strat mil's <pmilambo47@gmail.com>
2024-04-09 02:08:16 +0100

    job abd user administration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
+use App\Models\Company;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('index');
+        
+        $jobs=Job::all();
+        return view('index',compact('jobs'));
     }
     public function account()
     {
@@ -18,13 +22,17 @@ class HomeController extends Controller
     {
         return view('job-applied');
     }
-    public function job_detail()
+    public function job_detail(Request $request)
     {
-        return view('job-detail');
+        
+        $job=Job::find($request->id);
+        
+
+        return view('job-detail', compact('job'));
     }
     public function my_jobs()
     {
-        return view('my_jobs');
+        return view('my-jobs');
     }
     public function jobs()
     {
@@ -38,4 +46,36 @@ class HomeController extends Controller
     {
         return view('post-job');
     }
+    public function job_store(Request $request)
+    {
+        
+       
+        $Company=Company::create([
+            'name' => $request->company_name,
+            'location'=>$request->location,
+            'website' => $request->website
+        ]);
+        
+        Job::create([
+            'title'=>$request->title,
+            'category'=>$request->category,
+            'nature' => $request->time,
+            'vacancy'=> $request->vacancy,
+            'salary'=> $request->salary,
+            'location'=>$request->Location,
+            'description'=> $request->description,
+            'benefits' => $request->benefits,
+            'resposability'=> $request->resposability,
+            'qualifications' => $request->qualifications,
+            'keywords' => $request->keywords,
+            'Company_id'=>$Company->id,
+            'created_at'=>now()
+
+
+        ]);
+
+        return redirect('post-job');
+    }
+    
+
 }
